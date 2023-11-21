94ecf02af5995c9fc78ef24311159aa5ec7ed2a4
Sultanquasmi <67sajid231khan@gmail.com>
2023-11-21 19:42:34 +0530

    redux setup with ttolkit done

--- lms-front/src/main.jsx
+++ lms-front/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import store from "./redux/store.js";
 
-
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store = {store}>
   <BrowserRouter>
     <App />
-    </BrowserRouter>
-)
+    <Toaster  />
+  </BrowserRouter>
+  </Provider>
+);
